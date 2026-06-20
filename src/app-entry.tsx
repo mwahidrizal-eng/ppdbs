57f403c2e3506bf87347988958498b0f2f5856fc
mwahidrizal-eng <mwahidrizal@gmail.com>
2026-06-20 15:45:52 +0700

    Update import path for SettingsProvider

--- src/app-entry.tsx
+++ src/app-entry.tsx
@@ -2,7 +2,7 @@ import {StrictMode} from 'react';
 import {createRoot} from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
-import { SettingsProvider } from './context/SettingsContext';
+import { SettingsProvider } from './context/SettingsContext.tsx';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
